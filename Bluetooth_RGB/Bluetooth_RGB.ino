8d54eec63e1fb8a75e1ffd766bf1e935663afa3b
RAPHAEL LUIZ SCHETTINO <raphaelluiz128@hotmail.com>
2018-06-24 11:12:34 -0300

    Add files via upload

---
+++ Bluetooth_RGB/Bluetooth_RGB.ino
@@ -0,0 +1,54 @@
+#include <SoftwareSerial.h>
+
+int red = 13, green = 9, blue = 10;
+char rec;
+SoftwareSerial bluetoothBridge(2,3);
+
+void setup()
+{
+  bluetoothBridge.begin(9600);
+  bluetoothBridge.println("Connection established");
+  
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+}
+
+void loop()
+{
+  rec = bluetoothBridge.read();
+  switch(rec)
+  {
+    case '0':
+      TurnLedOn(red, "Red on");
+    break;
+    case '1':
+      TurnLedOff(red, "Red off");
+    break;
+    case '2':
+      TurnLedOn(green, "Green on");
+    break;
+    case '3':
+      TurnLedOff(green, "Green off");
+    break;
+    case '4':
+      TurnLedOn(blue, "Blue on");
+    break;
+    case '5':
+      TurnLedOff(blue, "Blue off");
+    break;
+  }
+}
+
+void TurnLedOn(int ledValue, const char* message)
+{
+  digitalWrite(ledValue, HIGH);
+  bluetoothBridge.println(message);
+}
+
+void TurnLedOff(int ledValue, const char* message)
+{
+  digitalWrite(ledValue, LOW);
+  bluetoothBridge.println(message);
+}
+
---
+++ Bluetooth_RGB/Bluetooth_RGB/Bluetooth_RGB.ino
@@ -0,0 +1,54 @@
+#include <SoftwareSerial.h>
+
+int red = 8, green = 9, blue = 10;
+char rec;
+SoftwareSerial bluetoothBridge(2,3);
+
+void setup()
+{
+  bluetoothBridge.begin(9600);
+  bluetoothBridge.println("Connection established");
+  
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+}
+
+void loop()
+{
+  rec = bluetoothBridge.read();
+  switch(rec)
+  {
+    case '0':
+      ligarLed(red, "Red on");
+    break;
+    case '1':
+      desligarLed(red, "Red off");
+    break;
+    case '2':
+      ligarLed(green, "Green on");
+    break;
+    case '3':
+      desligarLed(green, "Green off");
+    break;
+    case '4':
+      ligarLed(blue, "Blue on");
+    break;
+    case '5':
+      desligarLed(blue, "Blue off");
+    break;
+  }
+}
+
+void ligarLed(int ledValue, const char* message)
+{
+  digitalWrite(ledValue, HIGH);
+  bluetoothBridge.println(message);
+}
+
+void desligarLed(int ledValue, const char* message)
+{
+  digitalWrite(ledValue, LOW);
+  bluetoothBridge.println(message);
+}
+
